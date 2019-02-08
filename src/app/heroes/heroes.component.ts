3c008a7f316c35dddcf49d39680f628abb8f78b5
Miguel <miguel_144a@hotmail.com>
2019-02-08 17:17:46 -0600

    antes del branch

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
+import { HEROES } from '../mock-heroes';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +9,13 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
   
   hero: Hero = {
     id: 1,
     name: 'windstorm'
   };
+  
   constructor() { }
 
   ngOnInit() {
